8f8b92b5704f9805b703240c363f4ceb3c05101d
Trần Công Bình <tbinh2890@gmail.com>
2024-07-11 23:32:08 +0700

    add Abtrast.js

---
+++ Abstract.js
@@ -0,0 +1,23 @@
+class Board {
+    chessBoard
+
+    constructor(chessBoard) {
+        this.chessBoard = new chessBoard[8][8]
+    }
+
+    Move() {
+
+    }
+}
+
+class Player {
+    team
+
+    constructor(team) {
+        this.team = team
+    }
+
+    makeMove(Board) {
+
+    }
+}
