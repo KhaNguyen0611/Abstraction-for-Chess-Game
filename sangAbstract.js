a36967071e8113c046cd22f44b62051571338d17
nesgnas <sangbdt102@gmail.com>
2024-07-11 23:30:25 +0700

    Game abstract

---
+++ sangAbstract.js
@@ -0,0 +1,22 @@
+class Game {
+    playerBlack
+    playerWhite
+    isBlackTurn
+    boardGame
+
+    constructor(playerBlack, playerWhite, ) {
+        this.playerBlack = playerBlack;
+        this.playerWhite = playerWhite;
+        this.isBlackTurn = true
+        this.boardGame = this.initBoard();
+    }
+
+    initBoard(){}
+
+    startGame(){}
+
+}
+
+
+
+
